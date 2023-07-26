7d06b7dc51f3bc6b9538f68103f9fc917972dbda
Aleph Diallo <alephdiallo@mail.com>
2023-07-26 12:03:22 -0400

    Update server.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/server.js
+++ alephs_website/js/server.js
@@ -1,9 +1,14 @@
+let pagename = window.location.pathname;
 //gitserv = http, neo = https
 if(location.hostname == "aleph.is-local.org"){
   document.getElementById("gitserv").innerHTML = "<strong>HTTP</strong>";
-  document.getElementById("gitserv").src = "#";
+  document.getElementById("gitserv").href = "#";
+  document.getElementById("neo").innerHTML = "<strong>HTTPS</strong>";
+  document.getElementById("neo").href = "http://alephd.neocities.org/" + pagename;
 }
 if(location.hostname == "alephd.neocities.org"){
+  document.getElementById("gitserv").innerHTML = "<strong>HTTP</strong>";
+  document.getElementById("gitserv").href = "http://aleph.is-local.org/" + pagename;
   document.getElementById("neo").innerHTML = "<strong>HTTPS</strong>";
-  document.getElementById("neo").src = "#";
+  document.getElementById("neo").href = "#";
 }
