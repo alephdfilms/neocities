2273c9e2f449a2602459cfefef3cc1368db77dac
Aleph Diallo <alephdiallo@mail.com>
2023-07-26 11:40:17 -0400

    Update server.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/server.js
+++ alephs_website/js/server.js
@@ -1,26 +1,9 @@
 //gitserv = http, neo = https
-let httplink = document.getElementById("gitserv");
-let httpslink = document.getElementById("neo");
 if(location.hostname == "aleph.is-local.org"){
-  if (window.location.protocol == 'https:') {
-      
-    console.log("you are accessing us via "
-        + "not-http. "
-        + "Redirecting you to HTTP.");
-          
-    window.location.href =
-        window.location.href.replace(
-                'https:', 'http:');
-}
-else
-    (window.location.protocol == "https:") {
-        console.log("you are accessing us via"
-            + " our secure HTTPS protocol.");
-    }
-  httplink.innerHTML = "<strong>HTTP (GitHub Server)</strong>";
-  httplink.src = "#";
+  document.getElementById("gitserv").innerHTML = "<strong>HTTP</strong>";
+  document.getElementById("gitserv").src = "#";
 }
 if(location.hostname == "alephd.neocities.org"){
-  httpslink.innerHTML = "<strong>HTTPS (Neocities Server)</strong>";
-  httpslink.src = "#";
+  document.getElementById("neo").innerHTML = "<strong>HTTPS</strong>";
+  document.getElementById("neo").src = "#";
 }
