7399b547a94e205ef9feb7d8261550892bab05a2
Aleph Diallo <alephdiallo@mail.com>
2023-07-31 11:52:08 -0400

    Update contact.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/contact.js
+++ alephs_website/js/contact.js
@@ -1,9 +1,8 @@
 let contactbox = document.getElementById("contactbox");
 function emailform(){
 console.log("email form pulled up.");
-contactbox.innerHTML = "<form action='mailto:aleph@aleph.is-local.org' enctype='text/plain/' method='post'><label>Your username:</label><input type='text' name='name'><br><label>Your email address:</label><input type='email' name='mail'><br><label>Your comment:</label><br><input type='text' name='comment'><br><input type='submit' value='Send'> <input type='reset' value='Reset'></form>";
-}
-function clearbox(){
-console.log("cleared!");
-contactbox.innerHTML = "";
+window.open(
+  'contactform/email.html',
+  'popUpWindow',
+  'height=480,width=360,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 }
