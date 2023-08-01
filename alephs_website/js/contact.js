ca0eac4bc9cb75e39dac4ea5f739bbc0271e57f6
Aleph Diallo <alephdiallo@mail.com>
2023-07-31 21:07:04 -0400

    Delete contact.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/contact.js
+++
@@ -1,7 +0,0 @@
-function emailform(){
-console.log("email form pulled up.");
-window.open(
-  'contactform/email.html',
-  'popUpWindow',
-  'height=480,width=360,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
-}
