eb60afc3c64f8c20214dfbd952cc5ef7aac8c96f
Aleph Diallo <alephdiallo@mail.com>
2023-08-07 17:57:12 -0400

    Reupload entire site because, well, I can do that.
    yeah
    
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

---
+++ alephs_website/js/server.js
@@ -0,0 +1,14 @@
+let pagename = window.location.pathname;
+//gitserv = http, neo = https
+if(location.hostname == "aleph.is-local.org"){
+  document.getElementById("gitserv").innerHTML = "<strong>HTTP</strong>";
+  document.getElementById("gitserv").href = "#";
+  document.getElementById("neo").innerHTML = "HTTPS";
+  document.getElementById("neo").href = "http://alephd.neocities.org" + pagename;
+}
+if(location.hostname == "alephd.neocities.org"){
+  document.getElementById("gitserv").innerHTML = "HTTP";
+  document.getElementById("gitserv").href = "http://aleph.is-local.org" + pagename;
+  document.getElementById("neo").innerHTML = "<strong>HTTPS</strong>";
+  document.getElementById("neo").href = "#";
+}
