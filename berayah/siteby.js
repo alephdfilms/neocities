dd172cb0eb534a0a764bb765b6c5c48e4e585746
Aleph Diallo <alephdiallo@mail.com>
2023-08-09 09:27:51 -0400

    Update siteby.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- berayah/siteby.js
+++ berayah/siteby.js
@@ -1,11 +1,12 @@
-var amount = 7
+var amount = 8
 var sitebytext = new Array(amount)
 let random = Math.floor(Math.random() * amount) + 1
-sitebytext[0]="website by <a href='https://alephd.neocities.org' class='alephdlink'>alephd.neocities.org</a>"
-sitebytext[1]="<a href='https://alephd.neocities.org' class='alephdlink'>aleph was here</a> :)"
-sitebytext[2]="<a href='https://alephd.neocities.org' class='alephdlink'>alephd.neocities.org</a> is responsible for the site."
-sitebytext[3]="<a href='https://alephd.neocities.org' class='alephdlink'>aleph is safe from elimination</a>."
-sitebytext[4]="<a href='https://alephd.neocities.org' class='alephdlink'>mmm delicious, web snitch knishes</a>."
-sitebytext[5]="<a href='https://alephd.neocities.org' class='alephdlink'>like what you see? click here</a>."
-sitebytext[6]="<a href='https://alephd.neocities.org' class='alephdlink'>free html frames @ alephd, mmm :)</a>."
+sitebytext[1]="website by <a href='https://alephd.neocities.org' class='alephdlink'>alephd.neocities.org</a>"
+sitebytext[2]="<a href='https://alephd.neocities.org' class='alephdlink'>aleph was here</a> :)"
+sitebytext[3]="<a href='https://alephd.neocities.org' class='alephdlink'>alephd.neocities.org</a> is responsible for the site."
+sitebytext[4]="<a href='https://alephd.neocities.org' class='alephdlink'>aleph is safe from elimination</a>."
+sitebytext[5]="<a href='https://alephd.neocities.org' class='alephdlink'>mmm delicious, web snitch knishes</a>."
+sitebytext[6]="<a href='https://alephd.neocities.org' class='alephdlink'>like what you see? click here</a>."
+sitebytext[7]="<a href='https://alephd.neocities.org' class='alephdlink'>free html frames @ alephd, mmm :)</a>."
+sitebytext[8]="undefined"
 document.write(sitebytext[random]);
