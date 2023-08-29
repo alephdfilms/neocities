bfb5b4de972b635d59d1ccf2964fdf7b26472409
Aleph Diallo <alephdiallo@mail.com>
2023-08-29 17:55:37 -0400

    Add files via upload
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

---
+++ alephs_website/js/loadbuttons.js
@@ -0,0 +1,10 @@
+//Import buttons
+var button1 = "<a href='/index.html'><img src='/images/buttons/home.svg' alt='homepage' height='100%'></a>";
+var button2 = "<a href='/cartoons.html'><img src='/images/buttons/cartoons.svg' alt='cartoons' height='100%'></a>";
+var button3 = "<a href='/artbook.html'><img src='/images/buttons/artbook.svg' alt='artbook' height='100%'></a>";
+var button4 = "<a href='/games.html'><img src='/images/buttons/games.svg' alt='games' height='100%'></a>";
+var button5 = "<a href='/work.html'><img src='/images/buttons/work.svg' alt='work' height='100%'></a>";
+var button6 = "<a href='/about.html'><img src='/images/buttons/about.svg' alt='about' height='100%'></a>";
+var button7 = "<a href='mailto:alephdiallo@mail.com'><img src='/images/buttons/email.svg' alt='email' height='100%'></a>";
+var buttonspan = document.getElementById("buttons");
+buttonspan.innerHTML = button1 + button2 + button3 + button4 + button5 + button6 + button7;
