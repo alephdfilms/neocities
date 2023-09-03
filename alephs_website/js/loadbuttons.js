ff066adba8821d5d1cba1f50839f4f3784b56db0
Aleph Diallo <alephdiallo@mail.com>
2023-09-03 13:08:06 -0400

    Update loadbuttons.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/loadbuttons.js
+++ alephs_website/js/loadbuttons.js
@@ -1,10 +1,11 @@
 //Import buttons
-var button1 = "<a href='/index.html'><img src='/images/buttons/home.svg' alt='homepage' height='100%'></a>";
-var button2 = "<a href='/cartoons.html'><img src='/images/buttons/cartoons.svg' alt='cartoons' height='100%'></a>";
-var button3 = "<a href='/artbook.html'><img src='/images/buttons/artbook.svg' alt='artbook' height='100%'></a>";
-var button4 = "<a href='/games.html'><img src='/images/buttons/games.svg' alt='games' height='100%'></a>";
-var button5 = "<a href='/work.html'><img src='/images/buttons/work.svg' alt='work' height='100%'></a>";
-var button6 = "<a href='/about.html'><img src='/images/buttons/about.svg' alt='about' height='100%'></a>";
-var button7 = "<a href='mailto:alephdiallo@mail.com'><img src='/images/buttons/email.svg' alt='email' height='100%'></a>";
+var button1 = "<a href='/index.html'><img src='/images/buttons/home.svg' alt='homepage' width='auto' height='100%'></a>";
+var button2 = "<a href='/cartoons.html'><img src='/images/buttons/cartoons.svg' alt='cartoons' width='auto' height='100%'></a>";
+var button3 = "<a href='/artbook.html'><img src='/images/buttons/artbook.svg' alt='artbook' width='auto' height='100%'></a>";
+var button4 = "<a href='/games.html'><img src='/images/buttons/games.svg' alt='games' width='auto' height='100%'></a>";
+var button5 = "<a href='/work.html'><img src='/images/buttons/work.svg' alt='work' width='auto' height='100%'></a>";
+var button6 = "<a href='/goodies.html'><img src='/images/buttons/downloads.svg' width='auto' alt='email' height='100%'></a>";
+var button7 = "<a href='/about.html'><img src='/images/buttons/about.svg' alt='about' width='auto' height='100%'></a>";
+var button8 = "<a href='mailto:alephdiallo@mail.com'><img src='/images/buttons/email.svg' width='auto' alt='email' height='100%'></a>";
 var buttonspan = document.getElementById("buttons");
-buttonspan.innerHTML = button1 + button2 + button3 + button4 + button5 + button6 + button7;
+buttonspan.innerHTML = button1 + button2 + button3 + button4 + button5 + button6 + button7 + button8;
