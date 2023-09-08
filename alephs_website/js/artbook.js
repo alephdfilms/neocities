d5d883319c270c99ced4c0f02417f8fb6504a62f
Aleph Diallo <alephdiallo@mail.com>
2023-09-08 15:15:40 -0400

    Update artbook.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/artbook.js
+++ alephs_website/js/artbook.js
@@ -1,13 +1,18 @@
 let artbook_selection = document.getElementById("artbook_selection");
 let artbook_art_picture = document.getElementById("artbook_art_picture");
 var latestbutton = "<button onClick='latest();'>&starf;</button>"
-var artbook1 = "<button onClick='chooseart('atwcloth.png');'>1</button>";
+var artbook1 = "<a href='#atwcloth'>1</button>";
+
 function latest() {
 artbook_art_picture.src = "images/blah_guy.svg";
+artbook_art_picture.height = "32";
 }
-function chooseart(img) {
-artbook_art_picture.src = "images/art/" + img;
-}
+let hash = window.location.hash;
+if (hash) {
+  // Fragment exists
+artbook_art_picture.src = "images/art/" + hash;
+};
+
 artbook_selection.innerHTML = latestbutton + artbook1;
 
 latest();
