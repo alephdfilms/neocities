8d1f612c4bcffa3d78683bd54e3e8054db2c60a9
Aleph Diallo <alephdiallo@mail.com>
2023-09-08 15:03:28 -0400

    Create artbook.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

---
+++ alephs_website/js/artbook.js
@@ -0,0 +1,11 @@
+let artbook_selection = document.getElementById("artbook_selection");
+let artbook_art_picture = document.getElementById("artbook_art_picture");
+var latestbutton = "<button onClick='latest();'>&starf;</button>"
+var artbook1 = "<button onClick='chooseart(atwcloth.png);'>&starf;</button>";
+function latest() {
+artbook_art_picture.src = "images/art/atwcloth.png";
+}
+function chooseart(img) {
+artbook_art_picture.src = "images/art/" + img;
+}
+artbook_selection.innerHTML = latestbutton + artbook1;
