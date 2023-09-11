8cadc7a80302daebb8a6f3fd535377f271b02eac
Aleph Diallo <alephdiallo@mail.com>
2023-09-11 17:49:06 -0400

    Update artbook.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- alephs_website/js/artbook.js
+++ alephs_website/js/artbook.js
@@ -3,10 +3,10 @@ let artbook_selection = document.getElementById("artbook_selection");
 let artbook_art_picture = document.getElementById("artbook_art_picture");
 //add buttons
 var latestbutton = "<button onClick='latest();'>&starf;</button> | "
-var artbook1 = "<a href='?art=atwcloth'>1</a> ";
-var artbook2 = "<a href='?art=Untitled24'>2</a> ";
-var artbook3 = "<a href='?art=16bit'>3</a> ";
-var artbook4 = "<a href='?art=256'>4</a> ";
+var artbook1 = "<a href='?art=atwcloth.png'>1</a> ";
+var artbook2 = "<a href='?art=Untitled24.png'>2</a> ";
+var artbook3 = "<a href='?art=16bit.png'>3</a> ";
+var artbook4 = "<a href='?art=256.png'>4</a> ";
 //add params
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -20,7 +20,7 @@ artbook_art_picture.src = "images/art/256.png";
 
 let url = window.location.href;
 if(url.includes('?')){
-  artbook_art_picture.src = "images/art/" + art + ".png";
+  artbook_art_picture.src = "images/art/" + art;
 }else{
   artbook_art_picture.src = "images/blah_guy.svg";
   artbook_art_picture.width = 128;
