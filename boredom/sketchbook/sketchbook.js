671241cbe1c90efaec8355179a24ab9a665585d9
Aleph Diallo <alephdiallo@mail.com>
2023-10-01 13:16:04 -0400

    Create sketchbook.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

---
+++ boredom/sketchbook/sketchbook.js
@@ -0,0 +1,16 @@
+function latestchardesign() {
+//automatically go to the latest design of a character
+document.getElementById("latest").href = "frames/char/stuart/design_0001.html";
+}
+function latestgraphicdesign() {
+//automatically go to the latest graphic design project
+document.getElementById("latest").href = null;
+}
+function latestcomic() {
+//automatically go to the latest comic
+document.getElementById("latest").href = null;
+}
+function latestsketch() {
+//automatically go to the latest sketch
+document.getElementById("latest").href = null;
+}
