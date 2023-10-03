c6db8b74935b237e6d622abf707f3f6ed46acad2
Aleph Diallo <alephdiallo@mail.com>
2023-10-03 07:31:56 -0400

    Update sketchbook.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- boredom/sketchbook/sketchbook.js
+++ boredom/sketchbook/sketchbook.js
@@ -1,3 +1,31 @@
+if (/msie/i.test (navigator.userAgent)) //only override IE
+{
+	document.nativeGetElementById = document.getElementById;
+	document.getElementById = function(id)
+	{
+		var elem = document.nativeGetElementById(id);
+		if(elem)
+		{
+			//make sure that it is a valid match on id
+			if(elem.attributes['id'].value == id)
+			{
+				return elem;
+			}
+			else
+			{
+				//otherwise find the correct element
+				for(var i=1;i<document.all[id].length;i++)
+				{
+					if(document.all[id][i].attributes['id'].value == id)
+					{
+						return document.all[id][i];
+					}
+				}
+			}
+		}
+		return null;
+	};
+}
 function latestchardesign() {
 //automatically go to the latest design of a character
 document.getElementById("latest").href = "../frames/char/blah/design_0001.html";
