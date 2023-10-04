c83483f61a16dcf3338e8e10d2cc0b2d42c9c9bf
Aleph Diallo <alephdiallo@mail.com>
2023-10-04 07:32:06 -0400

    Update isitsafe.js
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- boredom/isitsafe.js
+++ boredom/isitsafe.js
@@ -1,59 +1,33 @@
-// Forked from https://codepen.io/gapcode/pen/vEJNZN
-
-// Get IE or Edge browser version
-var version = detectIE();
-var isitsafe = null;
-
-/**
- * detect IE
- * returns version of IE or false, if browser is not Internet Explorer
- */
-function detectIE() {
-  var ua = window.navigator.userAgent;
-
-  // Test values; Uncomment to check result …
-
-  // IE 10
-  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
-  
-  // IE 11
-  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
-  
-  // Edge 12 (Spartan)
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
-  
-  // Edge 13
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
-
-  var msie = ua.indexOf('MSIE ');
-  if (msie > 0) {
-    // IE 10 or older => return version number
-    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    var isitsafe = "nope";
-  }
-
-  var trident = ua.indexOf('Trident/');
-  if (trident > 0) {
-    // IE 11 => return version number
-    var rv = ua.indexOf('rv:');
-    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    var isitsafe = "nope";
-  }
-
-  var edge = ua.indexOf('Edge/');
-  if (edge > 0) {
-    // Edge (IE 12+) => return version number
-    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-    // Edge is actually nice with boredom
-  }
-
-  // other browser
-  return false;
+// if it isn't safe...
+function notsafe() {
+window.location.replace('/notsupported.html');
 }
 
-if(isitsafe !== null || isitsafe == null || isitsafe === null || isitsafe = null) {
-alert("You are using Internet Explorer.");
-window.location.replace('/notsupported.html');
-} else {
-ok
+if (window.document.documentMode) {
+notsafe();
+}
+//if the above does not work
+var ua = window.navigator.userAgent;
+var isIE = /MSIE|Trident|Edge\//.test(ua);
+if ( isIE ) {
+notsafe();
+}
+function isCrappyIE() {
+    var ua = window.navigator.userAgent;
+    var crappyIE = false;
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {// IE 10 or older => return version number        
+        crappyIE = true;
+    }
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {// IE 11 => return version number        
+        crappyIE = true;
+    }
+    return crappyIE;
+}   
+//if it REALLY sucks
+if(!isCrappyIE()) {
+  //
+}else {
+notsafe();
 }
