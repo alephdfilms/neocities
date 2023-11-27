7e664a54117569ef6eaa37e697b1ae063f4a16f4
Aleph Diallo <alephdiallo@mail.com>
2023-11-26 22:04:31 -0500

    I Don't Use Semicolons!
    Signed-off-by: Aleph Diallo <alephdiallo@mail.com>

--- berayah/siteby.js
+++ berayah/siteby.js
@@ -1,12 +1,12 @@
-var amount = 8
-var sitebytext = new Array(amount)
-let random = Math.floor(Math.random() * amount) + 1
-sitebytext[1]="website by <a href='https://boredom.rubberocket.com' class='alephdlink'>boredom.rubberocket.com</a>"
-sitebytext[2]="<a href='https://boredom.rubberocket.com' class='alephdlink'>aleph was here</a> :)"
-sitebytext[3]="<a href='https://boredom.rubberocket.com' class='alephdlink'>boredom, inc</a> is responsible for the site."
-sitebytext[4]="<a href='https://boredom.rubberocket.com' class='alephdlink'>aleph is safe from elimination</a>."
-sitebytext[5]="<a href='https://boredom.rubberocket.com' class='alephdlink'>mmm delicious, web snitch knishes</a>."
-sitebytext[6]="<a href='https://boredom.rubberocket.com' class='alephdlink'>like what you see? click here</a>."
-sitebytext[7]="undefined"
-sitebytext[8]="null"
+var amount = 8;
+var sitebytext = new Array(amount);
+let random = Math.floor(Math.random() * amount) + 1;
+sitebytext[1]="website by <a href='https://rubberocket.com' class='alephdlink'>website by rubberocket.com</a>";
+sitebytext[2]="<a href='https://rubberocket.com' class='alephdlink'>aleph was here</a> :)";
+sitebytext[3]="<a href='https://rubberocket.com' class='alephdlink'>rubberocket</a> is responsible for the site.";
+sitebytext[4]="<a href='https://rubberocket.com' class='alephdlink'>just who created this mess?!</a>.";
+sitebytext[5]="<a href='https://rubberocket.com' class='alephdlink'>like what you see? click here</a>.";
+sitebytext[6]="undefined";
+sitebytext[7]="null";
+sitebytext[8]="lull";
 document.write(sitebytext[random]);
